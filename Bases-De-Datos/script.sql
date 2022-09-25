1d3de0b3a8a1f604c6cf4aef5e1d9d7706161305
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-09-25 16:38:52 -0300

    Me faltaba agregar la tabla intermedia en el script (Resuelto)

--- Bases-De-Datos/script.sql
+++ Bases-De-Datos/script.sql
@@ -44,3 +44,15 @@ ALTER fecha_de_creacion SET DEFAULT (CURRENT_TIMESTAMP());
 
 ALTER TABLE peliculas_series
 ALTER calificacion SET DEFAULT (0);
+
+CREATE TABLE IF NOT EXISTS personajes_peliculas_series(
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    id_personaje INT UNSIGNED NOT NULL,
+    id_pelicula_serie INT UNSIGNED NOT NULL,
+    CONSTRAINT PK_personajes_peliculas_series_id PRIMARY KEY(id),
+    CONSTRAINT FK_personajes_peliculas_series_id_personaje FOREIGN KEY(id_personaje) REFERENCES personajes(id),
+    CONSTRAINT FK_personajes_peliculas_series_id_pelicula_serie FOREIGN KEY(id_pelicula_serie) REFERENCES peliculas_series(id)
+);
+
+ALTER TABLE personajes_peliculas_series
+ADD CONSTRAINT UQ_personajes_peliculas_series_id_personaje_id_pelicula_serie UNIQUE (id_personaje, id_pelicula_serie);
