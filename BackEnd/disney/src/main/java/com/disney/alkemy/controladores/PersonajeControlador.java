20c5d446c3aa4c8758fc14a1b82d6b20923a99de
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-10-03 15:52:51 -0300

    BackEnd. Creando los controladores

---
+++ BackEnd/disney/src/main/java/com/disney/alkemy/controladores/PersonajeControlador.java
@@ -0,0 +1,17 @@
+package com.disney.alkemy.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Leandro Deferrari
+ */
+
+@RestController
+@RequestMapping("/characters")
+public class PersonajeControlador {
+
+    
+    
+}
