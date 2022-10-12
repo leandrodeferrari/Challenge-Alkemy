5a04ad82444a30ee510a5d6cfe67fb02e968d617
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-10-12 01:15:24 -0300

    BackEnd. Creando la configuracion de Spring Security

---
+++ BackEnd/disney/src/main/java/com/disney/alkemy/configuraciones/SeguridadWebConfiguracion.java
@@ -0,0 +1,25 @@
+package com.disney.alkemy.configuraciones;
+
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author Leandro Deferrari
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SeguridadWebConfiguracion extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        
+        http.httpBasic(withDefaults()).authorizeHttpRequests().anyRequest().authenticated();
+        
+    }
+    
+}
