2a0815d0dc4b74de4cd844fad9e8f8d57cecd5cf
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-10-13 00:14:57 -0300

    BackEnd. Agregando configuraciones de Swagger, para la autenticacion

--- BackEnd/disney/src/main/java/com/disney/alkemy/configuraciones/SwaggerConfiguracion.java
+++ BackEnd/disney/src/main/java/com/disney/alkemy/configuraciones/SwaggerConfiguracion.java
@@ -1,15 +1,21 @@
 package com.disney.alkemy.configuraciones;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 
 /**
  *
@@ -28,10 +34,33 @@ public class SwaggerConfiguracion {
                 .apis(RequestHandlerSelectors.basePackage("com.disney.alkemy.controladores"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(getApiInfo());
+                .apiInfo(getApiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
 
     }
 
+    private ApiKey apiKey(){
+        
+        return new ApiKey("JWT", "Authorization", "header");
+        
+    }
+    
+    private SecurityContext securityContext(){
+        
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+        
+    }
+    
+    private List<SecurityReference> defaultAuth(){
+        
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        
+    }
+    
     private ApiInfo getApiInfo() {
 
         return new ApiInfo(
