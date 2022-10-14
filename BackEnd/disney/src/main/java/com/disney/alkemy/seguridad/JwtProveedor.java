fb47d8ab9a36f1d0636ff2394160a5756889c86c
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-10-13 21:56:07 -0300

    BackEnd. Borrando y acomodando las clases, del paquete seguridad

--- BackEnd/disney/src/main/java/com/disney/alkemy/seguridad/JwtProveedor.java
+++ BackEnd/disney/src/main/java/com/disney/alkemy/seguridad/JwtProveedor.java
@@ -4,22 +4,16 @@ import io.jsonwebtoken.Jwts;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
-
-//import org.springframework.http.HttpStatus;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.MalformedJwtException;
-//import io.jsonwebtoken.ExpiredJwtException;
-//import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
-//import io.jsonwebtoken.UnsupportedJwtException;
 
 /**
  *
@@ -53,37 +47,31 @@ public class JwtProveedor {
     }
 
     public String getEmailJWT(String token) {
+        
         Claims claims = Jwts.parser().setSigningKey(secreto).parseClaimsJws(token).getBody();
-		return claims.getSubject();
+        
+        return claims.getSubject();
+        
     }
 
-//    public boolean validateToken(String token) {
-//		try {
-//			Jwts.parser().setSigningKey(secreto).parseClaimsJws(token);
-//			return true;
-//		}catch (MalformedJwtException ex) {
-//			throw new BlogAppException(HttpStatus.BAD_REQUEST,"Token JWT no valida");
-//		}
-//		catch (ExpiredJwtException ex) {
-//			throw new BlogAppException(HttpStatus.BAD_REQUEST,"Token JWT caducado");
-//		}
-//		catch (UnsupportedJwtException ex) {
-//			throw new BlogAppException(HttpStatus.BAD_REQUEST,"Token JWT no compatible");
-//		}
-//		catch (IllegalArgumentException ex) {
-//			throw new BlogAppException(HttpStatus.BAD_REQUEST,"La cadena claims JWT esta vacia");
-//		}
-//	}
-    
     public boolean validateToken(String authToken) {
-		try {
-			Jws<Claims> claims = Jwts.parser().setSigningKey(secreto).parseClaimsJws(authToken);
-			return true;
-		} catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
-			throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
-		} catch (ExpiredJwtException ex) {
-			throw ex;
-		}
-	}
-    
+
+        try {
+
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secreto).parseClaimsJws(authToken);
+
+            return true;
+            
+        } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+            
+            throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
+            
+        } catch (ExpiredJwtException ex) {
+            
+            throw ex;
+            
+        }
+        
+    }
+
 }
