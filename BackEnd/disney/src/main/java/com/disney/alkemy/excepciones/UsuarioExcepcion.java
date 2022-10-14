9cd78e8f0e14aed35986608dced9d9bba999a33f
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-10-13 21:44:13 -0300

    BackEnd. Creando una clase excepción para Usuario

---
+++ BackEnd/disney/src/main/java/com/disney/alkemy/excepciones/UsuarioExcepcion.java
@@ -0,0 +1,16 @@
+package com.disney.alkemy.excepciones;
+
+/**
+ *
+ * @author Leandro Deferrari
+ */
+
+public class UsuarioExcepcion extends RuntimeException {
+
+    public UsuarioExcepcion(String mensaje) {
+
+        super(mensaje);
+
+    }
+
+}
